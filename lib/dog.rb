0f788b7a28ce07fafc5fc14c4a604d85ab4724c2
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-29 04:30:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -93,12 +93,12 @@ end
   end
 
   def update
-  sql = <<-SQL
-  UPDATE dogs SET name = ?, breed = ? WHERE id = ?
-  SQL
+    sql = <<-SQL
+    UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+    SQL
 
-  DB[:conn].execute(sql, self.name, self.breed, self.id)
-end
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
 
 
 end
